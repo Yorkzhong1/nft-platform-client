e5b33b2a9c86ba91d31354b5d18c2de775001ba5
Yorkzhong1 <yorkzhong@gmail.com>
2023-03-10 17:59:49 -0600

    Update _app.js

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,5 +1,11 @@
 import '@/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
+ 
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </Head>
   return <Component {...pageProps} />
 }
