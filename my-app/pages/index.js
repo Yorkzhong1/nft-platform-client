3c03779b8e191b8fdcd22ccee8c498b13072252f
Yorkzhong1 <yorkzhong@gmail.com>
2023-03-11 16:19:08 -0600

    utils updated

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
+import Web3Modal from "web3modal";
+import { getProviderOrSigner } from "./utils";
 
 
 
