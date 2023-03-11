85b513976e43e571d8f65133ed8481455dbc4825
Yorkzhong1 <yorkzhong@gmail.com>
2023-03-11 15:57:29 -0600

    Update index.js

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,8 +1,6 @@
-import { Contract, providers, utils } from "ethers";
 import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import Web3Modal from "web3modal";
-import axios from "axios"
 import styles from "../styles/Home.module.css";
 import { getProviderOrSigner } from "./utils";
 
@@ -43,53 +41,6 @@ export default function Home() {
   }, [walletConnected]);
 
 
-  const renderButton = () => {
-    // If wallet is not connected, return a button which allows them to connect their wallet
-    if (!walletConnected) {
-      return (
-        <button onClick={connectWallet} className={styles.button}>
-          Connect your wallet
-        </button>
-      );
-    }
-    // If we are currently waiting for something, return a loading button
-    if (loading) {
-      return <button className={styles.button}>Loading...</button>;
-    }
-
-
-    if (buttonFunction==1){
-      return(
-          <div> 
-              FileUpload
-          </div>
-          
-      )
-    }
-
-    if (buttonFunction==2){
-      return(
-        <div> 
-            Mint
-        </div>
-        
-      )
-    }
-
-    if (buttonFunction==3){
-      return(<div className="text-center">
-           <div className="m-5"><a href="https://testnets.opensea.io/collection/mumbai-creatures-v2"  target="_blank" role="button" aria-pressed="true"><h4>OpenSea</h4></a></div> 
-           <div className="m-5"><a href="https://blur.io/collections"  target="_blank"  role="button" aria-pressed="true"><h4>Blur</h4></a></div> 
-      </div>)
-    }
-
-    if (buttonFunction==4){
-      return(<div>使用说明</div>)
-    }
-
-   
-  };
-
   return (
     <div>
       <Head>
@@ -99,20 +50,19 @@ export default function Home() {
       </Head>
       <div className="container">
            <div className="row mt-5">
-              <div className="col-1 bg-dark m-1">
-              <div className="d-grid gap-2">
-                  <div className='mt-3 text-left'></div>
-                  <button type="button" className="btn btn-dark mt-5" onClick={()=>{setButtonFunction(1)}}>部署合约</button>
-                  <button type="button" className="btn btn-dark mt-5" onClick={()=>{setButtonFunction(2)}}>Mint NFT</button>
-                  <button type="button" className="btn btn-dark mt-5" onClick={()=>{setButtonFunction(3)}}>NFT市场</button>
-                  <button type="button" className="btn btn-dark mt-5" onClick={()=>{setButtonFunction(4)}}>使用说明</button>
+              <div id="menue" className="col-1 bg-dark m-1">
+                <div className="d-grid gap-2 mt-5 mb-5">
+                    <div className='mt-3 text-left'></div>
+                    <button type="button" className="btn btn-dark mt-5" onClick={()=>{setButtonFunction(1)}}>部署合约</button>
+                    <button type="button" className="btn btn-dark mt-5" onClick={()=>{setButtonFunction(2)}}>Mint NFT</button>
+                    <button type="button" className="btn btn-dark mt-5" onClick={()=>{setButtonFunction(3)}}>NFT市场</button>
+                    <button type="button" className="btn btn-dark mt-5" onClick={()=>{setButtonFunction(4)}}>使用说明</button>
+                  </div>
                 </div>
-              </div>
-              <div className="col-10 bg-light m-1">
+              <div id="maindisplay" className="col-10 bg-light m-1">
                   <div className="text-center">
                       <h1 className={styles.title}>欢迎来到NFT世界!</h1>
                   </div>
-                  {renderButton()}
               </div>
           </div>
         </div>
