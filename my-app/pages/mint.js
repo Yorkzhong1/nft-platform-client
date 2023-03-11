9a2a434fa5abefbb23bb0991ed7b5ddf97d26c63
Yorkzhong1 <yorkzhong@gmail.com>
2023-03-11 17:12:59 -0600

    Mint

---
+++ my-app/pages/mint.js
@@ -0,0 +1,14 @@
+
+import Head from "next/head";
+import React, { useEffect, useRef, useState } from "react";
+import Web3Modal from "web3modal";
+export const Mint = () => {
+
+    return (
+        <div >
+            <div className="m-5 p-3 border border-dark border-1">Contracts</div>
+            <div className="m-5 p-3 border border-dark border-1">Alread Minted</div>
+            <div className="m-5 p-3 border border-dark border-1">Mint</div>
+        </div>
+      )
+}
