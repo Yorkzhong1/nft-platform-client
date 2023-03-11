66e9ad22403602a803f3f63a9ace49ddb6427171
Yorkzhong1 <yorkzhong@gmail.com>
2023-03-11 16:15:37 -0600

    Update index.js

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -5,7 +5,29 @@ import styles from "../styles/Home.module.css";
 
 
 export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const web3ModalRef = useRef();
   const [buttonFunction, setButtonFunction] = useState(1);
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "mumbai",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      const connectWallet = async () => {
+        try {
+          await getProviderOrSigner();
+          setWalletConnected(true);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      connectWallet();   
+    }
+  }, [walletConnected]);
 
   const renderButton = () => {
     
