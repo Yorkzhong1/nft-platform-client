b6f304425ff0151a1ffbd17b1b8fad80af1e0c97
Yorkzhong1 <yorkzhong@gmail.com>
2023-03-11 16:37:55 -0600

    c

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,8 +1,10 @@
+import { Contract, providers, utils } from "ethers";
 import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 // import { getProviderOrSigner } from "./utils";
+// import Mint from "./mint";
 
 export default function Home() {
   const [walletConnected, setWalletConnected] = useState(false);
@@ -40,27 +42,27 @@ export default function Home() {
 
 
   const getProviderOrSigner = async (needSigner = false) => {
-  const web3ModalRef = new Web3Modal({
-      network: "goerli",
-      providerOptions: {},
-      disableInjectedProvider: false,
-    })
-  
-  const provider = await web3ModalRef.connect();
-  const web3Provider = new providers.Web3Provider(provider);
+      const web3ModalRef = new Web3Modal({
+          network: "goerli",
+          providerOptions: {},
+          disableInjectedProvider: false,
+        })
+      
+      const provider = await web3ModalRef.connect();
+      const web3Provider = new providers.Web3Provider(provider);
 
-  // If user is not connected to the Mumbai network, let them know and throw an error
-  const { chainId } = await web3Provider.getNetwork();
-  if (chainId !== 1337) {
-    window.alert("Change the network to Mumbai");
-    throw new Error("Change network to Mumbai");
-  }
+      // If user is not connected to the Mumbai network, let them know and throw an error
+      const { chainId } = await web3Provider.getNetwork();
+      if (chainId !== 1337) {
+        window.alert("Change the network to Mumbai");
+        throw new Error("Change network to Mumbai");
+      }
 
-  if (needSigner) {
-    const signer = web3Provider.getSigner();
-    return signer;
-  }
-  return web3Provider;
+      if (needSigner) {
+        const signer = web3Provider.getSigner();
+        return signer;
+      }
+      return web3Provider;
 };
 
 
@@ -79,7 +81,7 @@ export default function Home() {
       if (buttonFunction==2){
         return(
           <div> 
-             2
+             
           </div>
           
         )
