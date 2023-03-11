ca8dc095b25df633d38f3135cb2160b6d9d692cd
Yorkzhong1 <yorkzhong@gmail.com>
2023-03-11 17:35:07 -0600

    Update index.js

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,4 +1,4 @@
-import { Contract, providers, utils } from "ethers";
+import { Contract, providers, utils,ethers } from "ethers";
 import Head from "next/head";
 import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
@@ -53,29 +53,7 @@ export default function Home() {
   };
 
 
-  const getProviderOrSigner = async (needSigner = false) => {
-      const web3ModalRef = new Web3Modal({
-          network: "goerli",
-          providerOptions: {},
-          disableInjectedProvider: false,
-        })
-      
-      const provider = await web3ModalRef.connect();
-      const web3Provider = new providers.Web3Provider(provider);
-
-      // If user is not connected to the Mumbai network, let them know and throw an error
-      const { chainId } = await web3Provider.getNetwork();
-      if (chainId !== 1337) {
-        window.alert("Change the network to Mumbai");
-        throw new Error("Change network to Mumbai");
-      }
-
-      if (needSigner) {
-        const signer = web3Provider.getSigner();
-        return signer;
-      }
-      return web3Provider;
-};
+ 
 
 
 
@@ -141,7 +119,30 @@ export default function Home() {
   );
 }
 
+//
+const getProviderOrSigner = async (needSigner = false) => {
+  const web3ModalRef = new Web3Modal({
+      network: "goerli",
+      providerOptions: {},
+      disableInjectedProvider: false,
+    })
+  
+  const provider = await web3ModalRef.connect();
+  const web3Provider = new providers.Web3Provider(provider);
 
+  // If user is not connected to the Mumbai network, let them know and throw an error
+  const { chainId } = await web3Provider.getNetwork();
+  if (chainId !== 1337) {
+    window.alert("Change the network to Mumbai");
+    throw new Error("Change network to Mumbai");
+  }
+
+  if (needSigner) {
+    const signer = web3Provider.getSigner();
+    return signer;
+  }
+  return web3Provider;
+};
 //Mint component
 const Mint = () => {
   const [contractData,setContractData] = useState('');
