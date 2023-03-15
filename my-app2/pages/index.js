dbf0491584eb537065c6d737cea412d77a819cdb
Yorkzhong1 <yorkzhong@gmail.com>
2023-03-14 19:48:56 -0600

    c

--- my-app2/pages/index.js
+++ my-app2/pages/index.js
@@ -4,7 +4,7 @@ import React, { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 import axios from "axios"
-import {createNFT,getNFT,deactiveNFT,removeNFT } from "./client.js"
+// import {createNFT,getNFT,deactiveNFT,removeNFT } from "./client.js"
 
 // import { getProviderOrSigner } from "./utils";
 
@@ -511,7 +511,67 @@ const FolderUpload = (prop) => {
 
 
 
+//functions to interact with server
 
+
+const test=async ()=>{
+  try{
+      // console.log(`${serverUrl}/addNFT`)
+      await axios.get(`${serverUrl}/test`)
+      .then((res)=>{console.log(res.data)})
+  }catch (error) {
+      console.log(error);
+    }
+}
+// test()
+
+const createNFT=async ()=>{
+  try{
+      // console.log(`${serverUrl}/addNFT`)
+      await axios.post(`${serverUrl}/addNFT`,newNFT)
+      .then((res)=>{console.log(res.data)})
+  }catch (error) {
+      console.log(error);
+    }
+}
+//   createNFT()
+
+//get list of active NFT, change the input to "true" or "false" to get active or inactive lists of NFT
+const getNFT=async (active)=>{
+  try{
+      await axios.post(`${serverUrl}/getNFT`,{active:active})
+      .then((res)=>{console.log(res.data)})
+  }catch (error) {
+      console.log(error);
+    }
+}
+
+//   getNFT(false) 
+
+
+//deActive a specific NFT
+
+const deactiveNFT=async (contractAdd)=>{
+  try{
+      await axios.post(`${serverUrl}/deActive`,{contractAdd:contractAdd})
+      .then((res)=>{console.log(res.data)})
+  }catch (error) {
+      console.log(error);
+    }
+}
+
+//   deactiveNFT('0x000')
+
+//   remove 
+const removeNFT=async (contractAdd)=>{
+  try{
+      await axios.post(`${serverUrl}/remove`,{contractAdd:contractAdd})
+      .then((res)=>{console.log(res.data)})
+  }catch (error) {
+      console.log(error);
+    }
+
+  }
 
 
 
