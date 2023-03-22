226578d77f6703b99f5cc69802a77e7128dea5eb
Yorkzhong1 <yorkzhong@gmail.com>
2023-03-22 10:50:27 -0600

    Update index.js

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -157,9 +157,7 @@ export default function NFT() {
           <div className="row">
               <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                  </button>
+     
                   <div className="collapse navbar-collapse d-flex" id="navbarTogglerDemo01">
                         <button type="button" className="btn btn-dark btn-sm mt-5 me-2" onClick={()=>{setButtonFunction(0)}}>HOME</button>
                         <button type="button" className="btn btn-dark btn-sm mt-5 me-2" onClick={()=>{setButtonFunction(1)}}>DEPLOY</button>
